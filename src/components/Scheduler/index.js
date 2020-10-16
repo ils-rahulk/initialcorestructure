55779b7391cc65691cd8c6d50cbeb74b3efd8ccf
Rahul Kashyap <r.kashyap@thesynapses.com>
2020-10-16 19:28:41 +0530

    feat: setup full calendar

--- src/components/Scheduler/index.js
+++ src/components/Scheduler/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { CRow, CCard, CCol, CButton } from '@coreui/react'
+import FullCalendar from '@fullcalendar/react'
+import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
+import { resourses, eventDetails } from './constant';
 const Scheduler = () => {
     return (
         <>
-            Coming Soon
+            <CCard>
+                <CRow>
+
+                    <CCol sm="6">
+                        <h4 className="tabsHeading">Schedule</h4>
+                    </CCol>
+                    <CCol sm="6" className="text-sm-right">
+                        <CButton onClick={ () => console.log('click') } color="primary" className="px-4">Today 08/08/21</CButton>
+                    </CCol>
+                </CRow>
+            </CCard>
+            <FullCalendar
+                plugins={ [ resourceTimelinePlugin  ] }
+                initialView="resourceTimeline"
+                resources= { resourses }
+                events={ eventDetails }
+                resourceLabelDidMount= { (renderInfo) => {
+                    renderInfo.el.style.backgroundColor = 'blue';
+                    renderInfo.el.style.color = 'white';
+                    // renderInfo.el.innerHTML =  renderInfo.el.innerText;
+                } }
+            />
         </>
     )
 }
